d048b98ea2dc295bb069527d634d7c06ba37d22e
underpostnet <development@underpost.net>
2025-08-01 14:12:01 +0000

    ci(engine-lampp-repo-build): ⚙️ Update engine lampp repository

--- src/cli/index.js
+++ src/cli/index.js
@@ -315,6 +315,13 @@ program
   .description('Manages health server monitoring for specified deployments.')
   .action(Underpost.monitor.callback);
 
+// 'run' command: Run a script
+program
+  .command('run')
+  .argument('[path]', 'The absolute or relative directory path where the script is located.')
+  .description('Runs a script from the specified path.')
+  .action(Underpost.run.callback);
+
 // 'lxd' command: LXD management
 program
   .command('lxd')
